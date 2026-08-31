48c842c18da883b3a717dbc5e3bd8061aed090ba
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-08-31 10:14:26 +0200

    mixing strings with ints

--- Program.cs
+++ Program.cs
@@ -16,4 +16,10 @@ int number2 = 2;
 
 int sumResult = number1 + number2;
 
-Console.WriteLine(sumResult);
+// Console.WriteLine(sumResult);
+
+// Console.WriteLine(5 + 5 + "2");
+
+// int sumIntsWithString = 5 + 5 + "2";
+
+// Console.WriteLine(sumIntsWithString);
