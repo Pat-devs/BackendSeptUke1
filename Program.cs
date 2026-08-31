da47c6fe9f96505cdc068fa3d49a197fcc7e334b
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-08-31 10:41:53 +0200

    boolean type quick note

--- Program.cs
+++ Program.cs
@@ -1,34 +1,7 @@
-﻿// variable declaration needs 3 thing:
-// type (is it a text, number, or whatever else ?)
-// a name (something we can refer to) 
-// assignment operator (single equals sign)
-// a value
+﻿string greeting = "hello" + " wonderful " + "world!";
 
-string greeting = "hello" + " wonderful " + "world!";
+// boolean (true/false)
 
-// Console.WriteLine(greeting);
+bool isItMonday = true;
 
-// numerical types:
-
-// integer (whole numbers)
-int number1 = 5;
-int number2 = 2;
-
-// + is sum
-// - is substract
-// * is multiplication
-// / is division
-
-Console.WriteLine(number1 / number2);
-
-// one extra fancy operator
-
-// modulo, or remainder operator is the % sign
-
-Console.WriteLine(number1 % number2);
-// explanation: 5 / 2 = 2... so 2 * 2 = 4 ... means there is 1 left over.
-
-Console.WriteLine(2 % 2);
-Console.WriteLine(3 % 2);
-Console.WriteLine(4 % 2);
-Console.WriteLine(5 % 2);
+Console.WriteLine(isItMonday);
