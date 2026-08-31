40645dfce3b47c0cc357fb704a9460d496f3ae65
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-08-31 09:59:29 +0200

    code comments

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Code comment (kode kommetar)
+Console.WriteLine("Hello, World!"); // Also a code comment
+
+
+/* (creates a block comment)
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+
+*/ // dont forget to close the block comment
