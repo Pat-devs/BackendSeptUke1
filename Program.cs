a511de2dd416e5d21116c747d4c6564bf918f290
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-08-31 11:07:35 +0200

    add a short note on naming convetion in C#

--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
-﻿string name = "Anna";
-int age = 20;
+﻿// basic greeter program
+// ask user for their name and then print their name
+// and then ask the user for their age
 
-// print a greeting, that says users name and their age in one line
+// inform the user what the program expects
+Console.WriteLine("Please enter your name, and then hit enter: ");
 
-Console.WriteLine("Hi, my name is " + name + " and i am " + age + " years old.");
+string userName = Console.ReadLine();
 
-Console.WriteLine($"Hi, my name is {name} and i am {age} years old.");
+Console.WriteLine($"Users name is {userName}");
+
+string userAge = Console.ReadLine();
+
+Console.WriteLine($"Users age is {userAge}");
+
+// small note on naming convetion in C#:
+
+// local variables are camelCase (name starts with an a-z value in lowercase), any any new word is split on an uppercase first character
+// Global or Public variables or Classes are PascalCase (name starts with an A-Z value in uppercase) , any any new word is split on an uppercase first character
+// Lastly, we usually prefer very verbose variablenames over short names that can confuse the reader.
