5b656fa8b48b47867e902a6f9cac48a2563edf3c
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-08-31 10:48:56 +0200

    declaring a char type

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿string greeting = "hello" + " wonderful " + "world!";
 
-Console.Write(greeting[1] + "" +  greeting[2]);
+// Console.Write(greeting[1] + "" +  greeting[2]);
+
+// creating a char type
+
+char menuOption = 'a';
+
+//Console.WriteLine(menuOption);
